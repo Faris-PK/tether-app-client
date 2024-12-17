d6420f0e4c0a4a0ecf15e1a9b2c2bf3fcb2fe8b5
Faris Pk <faripk369@gmail.com>
2024-12-17 14:54:01 +0530

    vercel issue solving

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js (CommonJS version)
+module.exports = {
 	darkMode: ["class"],
 	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
